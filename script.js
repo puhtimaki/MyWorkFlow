ee3dfa7c8ef7277d334593c2eb4692bd3e5e23ca
puhtimaki <113307512+puhtimaki@users.noreply.github.com>
2023-01-10 20:18:17 +0200

    keypresses

--- script.js
+++ script.js
@@ -9,11 +9,40 @@ async function makeAPICall() {
     document.getElementById(
       'apiweather',
     ).innerHTML = `It's ${temperature} °C in Vaasa`
-
-    console.log(`It's ${temperature} °C in Vaasa`)
   })
-
-  document.getElementById('weather').innerHTML = JSON.stringify(temperature)
 }
 
 makeAPICall()
+
+let myinput = document.getElementById('myinput')
+let mybtn = document.getElementById('push')
+myinput.addEventListener('keyup', (e) => {
+  e.preventDefault()
+  if (e.keyCode === 13) {
+    mybtn.click()
+  }
+})
+mybtn.addEventListener('click', () => {})
+document.querySelector('#push').onclick = function () {
+  if (document.querySelector('#newtask input').value.length == 0) {
+    alert('Please enter a task')
+  } else {
+    document.querySelector('#tasks').innerHTML += `
+    
+    <div class="task">
+    <span id ="taskname">
+    ${document.querySelector('#newtask input').value}</span>
+<button class = "delete">
+<i class="fa fa-check"></i></button>
+</div>`
+
+    document.querySelector('#myinput').value = ''
+
+    const current_tasks = document.querySelectorAll('.delete')
+    for (let i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove()
+      }
+    }
+  }
+}
