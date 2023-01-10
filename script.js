509ce3cb62068ad5ef3f8899ac3462f38b742e87
puhtimaki <113307512+puhtimaki@users.noreply.github.com>
2023-01-10 18:28:38 +0200

    fetch api complete

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
 'use strict'
 
-    const URL = "https://api.openweathermap.org/data/2.5/weather?lat=63.09&lon=21.610&units=metric&appid=b102d81092eef4a28e509bfe54a92a4d";
-    async function makeAPICall(){
-const result = await fetch(URL)
-result.json().then(data => {
-const temperature = data.main.temp;
+const URL =
+  'https://api.openweathermap.org/data/2.5/weather?lat=63.09&lon=21.610&units=metric&appid=b102d81092eef4a28e509bfe54a92a4d'
+async function makeAPICall() {
+  const result = await fetch(URL)
+  result.json().then((data) => {
+    const temperature = data.main.temp
+    document.getElementById(
+      'apiweather',
+    ).innerHTML = `It's ${temperature} °C in Vaasa`
 
+    console.log(`It's ${temperature} °C in Vaasa`)
+  })
 
-console.log(`It's ${temperature} °C in Vaasa`)})
+  document.getElementById('weather').innerHTML = JSON.stringify(temperature)
+}
 
-document.getElementById("weather").innerHTML = temperature;
-
-    }
-
-
-
-    makeAPICall();
-
-
-
-    //const mainTemp = document.getElementById('WeatherDiv').innerHTML = JSON.stringify(temperature);
-    //const divWeather = document.createElement("divWeather");
-    //divWeather.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
- 
-
-    //var div = document.createElement("div");
-    //div.innerHTML = tempp2;
-
-    //weatherFetch.appendChild(div);
-    //var weatherFetch = document.getElementById("weather");
+makeAPICall()
